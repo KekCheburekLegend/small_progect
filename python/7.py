62638a7745ba45a29ad99ab4f811c08390d2cdd0
Iskander Khanov <needdocer@gmail.com>
2025-10-29 00:27:41 +0500

    smile

--- python/7.py
+++ python/7.py
@@ -2,5 +2,7 @@ import time
 a = ''
 b = ['🚗','🎉','😒','🙌','🎶','😁','😂','🤣','😃',]
 for i in b:
-    time.sleep(1)
-    print(i, end = ' ')
+    time.sleep(0.1)
+    print(i)
+    for c in b:
+        print(i + c)
